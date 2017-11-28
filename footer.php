d9566d0a296bcfa4625d10101d4c96068f91a524
Claudia Zanetel <claudia.zanetel@gmail.com>
2017-11-28 21:39:14 +0100

    removed h4 in footer and added a margin

--- footer.php
+++ footer.php
@@ -1,8 +1,6 @@
 			<div class='row'>
-			    <div class='col-sm-10 col-sm-offset-1' id='main_offset'>	
-			    	
+			    <div class='col-sm-10 col-sm-offset-1 footer' id='main_offset>	
 			        <?php wp_footer(); ?>
-			        <h4>che puzza questi piedi</h4>
 			    </div>
 			</div>
 			 <!--   </div> closing main_offset-->
